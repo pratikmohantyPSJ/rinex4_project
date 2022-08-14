4117327142c3a2ed2240fd1af3ef10f52ade2b9b
pratikmohantyPSJ <108286742+pratikmohantyPSJ@users.noreply.github.com>
2022-08-14 13:11:31 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st 
+import joblib 
+
+#load the joblib model 
+model_nb = joblib.load('bmi-pred')
+
+#user input 
+st.title("BMI PREDICTOR")
+ip = st.text_input("Enter height:")
+ip = st.text_input("Enter weight:")
+
+#predict the bmi
+op = model_nb.predict([ip])
+if st.button('PREDICT'):
+  st.title(op[0])  
